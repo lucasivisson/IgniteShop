d09f51dfc6157cb69903d1bee521538caae442ff
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-28 19:53:43 -0300

    ✨ introduce SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router"
 import { useState } from "react";
@@ -37,31 +38,28 @@ export default function Product({ product }: ProductProps) {
     }
   }
 
-  const { isFallback } = useRouter();
-
-  if(isFallback) {
-    return (
-      <h2>Loading...</h2>
-    )
-  }
-
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt=""/>
-      </ImageContainer>
+    <>
+      <Head>
+          <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
+        </ImageContainer>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>{product.description}</p>
+          <p>{product.description}</p>
 
-        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+          <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
@@ -73,7 +71,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [
       { params: { id: 'prod_NmnrvRfnfyDNGB' } }
     ],
-    fallback: 'blocking'
+    fallback: true
   }
 }
 
