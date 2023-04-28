180aab2c1c3af88b88a3ce74c97eec7686851f55
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-28 16:53:46 -0300

    ✨ introduce buy button

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -2,7 +2,15 @@ import { stripe } from "@/lib/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  const priceId = 'price_1N1EFmDNA7Bi7NzQpmO4SUlO'
+  const { priceId } = request.body;
+
+  if(request.method !== 'POST') {
+    return response.status(405).json({ error: 'Method not allowed'});
+  }
+
+  if(!priceId) {
+    return response.status(400).json({ error: 'Price not found!'})
+  }
 
   const successUrl = `${process.env.NEXT_URL}/success`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
