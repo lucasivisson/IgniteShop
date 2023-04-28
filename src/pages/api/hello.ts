d839d33e5e981c95257fa00eb58493cf63f13d53
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-28 13:03:53 -0300

    🚧 working with api routes on next

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(request: NextApiRequest, response: NextApiResponse) {
+  return response.json({ message: 'Hello World' });
+}
